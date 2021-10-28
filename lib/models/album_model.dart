f46e6e298ae85f95bcfec1ba790697ac9ea1254b
Matthieu <matthieunadeau96@gmail.com>
2021-10-28 16:53:18 -0400

    Create Album Model

---
+++ lib/models/album_model.dart
@@ -0,0 +1,34 @@
+class Album {
+  final String? artistName;
+  final String? albumName;
+  final String? albumImage;
+  final double? albumPrice;
+  final DateTime? releaseDate;
+  bool isLiked = false;
+
+// artistName - (Artist Name)
+// collectionName - (Album Name)
+// artworkUrl160 - (Smaller Image)
+// artworkUrl1100 - (Larger Image)
+// collectionPrice - (Album Price)
+// releaseDate - (Album Release)
+
+  Album({
+    this.artistName,
+    this.albumName,
+    this.albumImage,
+    this.albumPrice,
+    this.releaseDate,
+    this.isLiked = false,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      artistName: json['artistName'] ?? 'Error',
+      albumName: json['collectionName'] ?? 'Error',
+      albumImage: json['artworkUrl100'] ?? '',
+      albumPrice: json['collectionPrice'] ?? 0.00,
+      releaseDate: DateTime.parse(json['releaseDate']),
+    );
+  }
+}
