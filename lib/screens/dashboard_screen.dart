7754bf932c8cd44ca80b4192d8536424c1b411ca
Matthieu <matthieunadeau96@gmail.com>
2021-10-28 17:10:35 -0400

    Add AlbumCard widget to Dashboard

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:fec_mount/services/album_service.dart';
+import 'package:fec_mount/widgets/album_card.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
@@ -8,11 +9,31 @@ class Dashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: TextButton(
-          onPressed: () async {
-            await AlbumService().getHttp();
+        child: FutureBuilder<Object>(
+          future: AlbumService().getHttp(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasData) {
+                return const Center(
+                  child: SizedBox(
+                    height: 200,
+                    // width: 200,
+                    child: AlbumCard(),
+                  ),
+                );
+              }
+              // TODO: Handle if no data
+
+            }
+            return const Center(
+              child: Text('Something went wrong...'),
+            );
           },
-          child: Text('Tap me'),
         ),
       ),
     );
