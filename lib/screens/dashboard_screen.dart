3cc28982404f6dae81b9f9a7275ed6d4b66066c1
Matthieu <matthieunadeau96@gmail.com>
2021-10-29 11:13:34 -0400

    Dashboard: make stateful to stop list rebuilds

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,21 +1,39 @@
 import 'package:fec_mount/models/album_model.dart';
+import 'package:fec_mount/models/collection_model.dart';
 import 'package:fec_mount/services/album_service.dart';
 import 'package:fec_mount/widgets/album_card.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
 
   @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  late Future<List<Album>> _future;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _future = AlbumService().getHttp();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    Collection _collection = Provider.of<Collection>(context);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('Total Likes: 0'),
+        title: Text('Total Likes: ${_collection.totalFavorites}'),
       ),
       body: Center(
         child: FutureBuilder<List<Album>>(
-          future: AlbumService().getHttp(),
+          future: _future,
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return const Center(
@@ -26,17 +44,17 @@ class Dashboard extends StatelessWidget {
               if (snapshot.hasData) {
                 List<Album> albumList = snapshot.data!;
 
+                _collection.albumList = albumList;
+
                 return ListView.builder(
-                    itemCount: albumList.length,
-                    itemBuilder: (context, index) {
-                      Album album = albumList[index];
-                      return AlbumCard(
-                        albumImage: album.albumImage!,
-                        albumName: album.albumName!,
-                        releaseDate: album.releaseDate!,
-                        albumPrice: album.albumPrice!,
-                      );
-                    });
+                  itemCount: albumList.length,
+                  itemBuilder: (context, index) {
+                    Album album = albumList[index];
+                    return AlbumCard(
+                      album: album,
+                    );
+                  },
+                );
               }
               // TODO: Handle if no data
 
